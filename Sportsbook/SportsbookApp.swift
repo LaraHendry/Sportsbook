abd04460ecb67e742be3312cbd7c27c5c416c8e4
LaraHendry <lara.hendry@xdesign.com>
2021-11-15 11:02:43 +0000

    Initial Commit

---
+++ Sportsbook/SportsbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsbookApp.swift
+//  Sportsbook
+//
+//  Created by Lara Hendry on 15/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SportsbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
